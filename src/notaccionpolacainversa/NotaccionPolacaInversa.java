61d6be3ad37a6ab2bb7326a693da7ae7621794a0
Mulay <Mulay@DESKTOP-1DQT6OU.home>
2021-02-26 14:40:44 +0100

    RPN

--- src/notaccionpolacainversa/NotaccionPolacaInversa.java
+++ src/notaccionpolacainversa/NotaccionPolacaInversa.java
@@ -11,11 +11,34 @@ package notaccionpolacainversa;
  */
 public class NotaccionPolacaInversa {
 
+    public String calculadoraRPN ( String [] entrada){
+        
+        String pila [] = new String [100];
+        int posicionPila = 0; //indica el sitio de la pila en el que tenemos que insetrar o leer
+        int posocionEntrada = 0; //indica el sitio de la entrada que estamos procesando
+        
+        //el length da la longitud del numero de elementos que me han pasado. Ejmplo abajo
+        System.out.println(entrada.length);
+        
+        for (int i=0; i< entrada.length; i++){
+            
+            if (entrada[i] != "+" && entrada[i] != "-" && entrada[i] != "*" && entrada[i] != "/"){
+                System.out.print(entrada[i] + " ");  
+            }
+        }
+        
+        
+        
+        
+        return " ";
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        NotaccionPolacaInversa ejercicio = new NotaccionPolacaInversa();
+                
+        ejercicio.calculadoraRPN( new String[]{"3", "2", "+", "7", "*", "15", "21", "+", "-"});
     }
     
 }
